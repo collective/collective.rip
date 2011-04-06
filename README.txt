41fab6e222936c3cc199d72710b25c76e46b3c9d
aclark <aclark@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-04-06 00:33:23 +0000

    Doc updates, prepare 1.0, and other tidying

--- README.txt
+++ README.txt
@@ -1,6 +1,34 @@
+
 Introduction
 ============
 
-For quick and dirty UI prototyping, install this add-on to edit CSS and 
+This is ``collective.rip`` AKA "Resources In Plone".
+
+For quick and dirty UI prototyping: install this add-on to edit CSS and 
 Javascript from within Plone (in the Plone control panel). You can also 
 toggle CSS/JS debug modes.
+
+Installation
+------------
+
+To install: 
+
+- Add ``collective.rip`` to your buildout ``plone.recipe.zope2instance``
+  section's eggs parameter, e.g.::
+
+    [plone]
+    recipe = plone.recipe.zope2instance
+    ...
+    eggs = 
+        ...
+        collective.rip
+
+- Run buildout and restart Plone.
+
+- Browse to Site Setup -> Add-ons -> Available add-ons -> [] Resources in Plone
+
+- Check the box next to ``Resources in Plone`` and click the Activate button.
+
+Have fun!
+
+Questions/comments/concerns? E-mail: aclark@aclark.net
--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.3'
+version = '1.0'
 
 setup(name='collective.rip',
       version=version,
       description="Use a text area in the Plone control panel to edit CSS and Javascript.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
@@ -17,7 +15,7 @@ setup(name='collective.rip',
       keywords='Developer tools, Theming',
       author='Alex Clark',
       author_email='aclark@aclark.net',
-      url='http://svn.plone.org/svn/collective/',
+      url='http://svn.plone.org/svn/collective/collective.rip',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
@@ -25,14 +23,9 @@ setup(name='collective.rip',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
       ],
       entry_points="""
-      # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
